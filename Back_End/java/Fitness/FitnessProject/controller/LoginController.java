aeba944ae0e8362c3711bb0f5014ae72465945c2
ShubhamSthul <sthul.shubham123@gmail.com>
2022-09-26 12:44:31 +0530

    update backend

---
+++ Back_End/java/Fitness/FitnessProject/controller/LoginController.java
@@ -0,0 +1,39 @@
+package Fitness.FitnessProject.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import Fitness.FitnessProject.models.Login;
+@RestController
+@CrossOrigin
+public class LoginController {
+    @Autowired
+	JdbcTemplate temp;
+    @PostMapping("/login")
+	public int CheckLogin(@RequestBody Login login) {
+          int id=0;
+		try {
+			 id = temp.queryForObject("select loginid from login where username='"+login.getUsername()+"' and password='"+login.getPassword()+"'",Integer.class);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return id;
+	}
+    
+    @GetMapping("/getrole/{id}")
+  	public String getRole(@PathVariable int id) {
+  			 return temp.queryForObject("select role from login where loginid="+id,String.class);
+  			
+  	}
+    
+   
+}
